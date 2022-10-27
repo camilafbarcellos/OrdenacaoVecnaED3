141e5a875d22ff421515aea91e80122a8c1ce3b4
camilafbarcellos <79776234+camilafbarcellos@users.noreply.github.com>
2022-10-26 21:16:22 -0300

    Ajuste no print de tempo

--- CancaoVecna.py
+++ CancaoVecna.py
@@ -143,5 +143,5 @@ if __name__ == '__main__':
     fim = time.time()
     print(
         '*                 > TEMPO DE EXECUÇÃO:', round(fim - inicio), 'SEGUNDOS <                *'
-                                                                       '\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *'
+        '\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *'
     )
